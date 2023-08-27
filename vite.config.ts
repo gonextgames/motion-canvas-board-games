e93ac06cc95e240c76722174eaccbd0835d02fc8
Oliver Barnum <oliverbarnum32@gmail.com>
2023-08-27 03:09:40 -0500

    Fix local package inclusion issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import {defineConfig} from 'vite';
 import motionCanvas from '@motion-canvas/vite-plugin';
 import ffmpeg from '@motion-canvas/ffmpeg';
+import * as path from "path";
 
 export default defineConfig({
   plugins: [
     motionCanvas(),
     ffmpeg(),
   ],
+  resolve: {
+    alias: {
+      '@motion-canvas/core': path.resolve('./node_modules/@motion-canvas/core'),
+      '@motion-canvas/2d': path.resolve('./node_modules/@motion-canvas/2d'),
+    },
+  },
 });
